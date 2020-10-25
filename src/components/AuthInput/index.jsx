fc1bb525184782bb29a32c3b97ca68cb3d2f26ce
Christian <alvescastro00cca13@gmail.com>
2020-10-25 18:14:31 -0300

    inputs

--- src/components/AuthInput/index.jsx
+++ src/components/AuthInput/index.jsx
@@ -1,50 +1,45 @@
-import React, { InputHTMLAttributes } from 'react';
+import React from 'react';
 
 import './styles.css';
 
-// interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-//   label: string;
-//   name: string;
-//   position?: string;
-//   password?: boolean;
-// }
 
-// const AuthInput: React.FC<InputProps> = ({ label, name, position, password, ...rest }) => {
 
-//   function labelFocus() {
-//     const labelText: any = document.querySelector(`#label${name}`);
-//     return labelText.className = 'focus';
-//   }
+const AuthInput = ({ label, name, position, password, ...rest }) => {
 
-//   function labelBlur() {
-//     const labelText: any = document.querySelector(`#label${name}`);
-//     const textInput = document.querySelector(`#${name}`) as HTMLInputElement;
-//     if (textInput.value === '')
-//       return labelText.className = 'unfocused';
-//   }
+  function labelFocus() {
+    const labelText = document.querySelector(`#label${name}`);
+    return labelText.className = 'focus';
+  }
 
-//   function buttonVisibility() {
-//     const button = document.querySelector(`#button${name}`) as HTMLSpanElement;
-//     const textInput = document.querySelector(`#${name}`) as HTMLInputElement;
-//     if (button.innerText === "visibility") {
-//       textInput.type = "text";
-//       button.innerText = "visibility_off";
-//     } else {
-//       textInput.type = "password"
-//       button.innerText = "visibility"
-//     }
-// }
+  function labelBlur() {
+    const labelText = document.querySelector(`#label${name}`);
+    const textInput = document.querySelector(`#${name}`);
+    if (textInput.value === '')
+      return labelText.className = 'unfocused';
+  }
+
+  function buttonVisibility() {
+    const button = document.querySelector(`#button${name}`);
+    const textInput = document.querySelector(`#${name}`);
+    if (button.innerText === "visibility") {
+      textInput.type = "text";
+      button.innerText = "visibility_off";
+    } else {
+      textInput.type = "password"
+      button.innerText = "visibility"
+    }
+}
   
 
-//   return (
-//     <div className="authInput-block">
-//       <label className="unfocused" id={`label${name}`} htmlFor={name} >{label}</label>
-//       <input onFocus={labelFocus} onBlur={labelBlur} className={position} id={name} {...rest} />
-//       {
-//         password === true ? <span onClick={buttonVisibility} id={`button${name}`} className="material-icons-outlined"> visibility </span> : false
-//       }
-//     </div>
-//   )
-// }
+  return (
+    <div className="authInput-block">
+      <label className="unfocused" id={`label${name}`} htmlFor={name} >{label}</label>
+      <input onFocus={labelFocus} onBlur={labelBlur} className={position} id={name} {...rest} />
+      {
+        password === true ? <span onClick={buttonVisibility} id={`button${name}`} className="material-icons-outlined"> visibility </span> : false
+      }
+    </div>
+  )
+}
 
 export default AuthInput;
