429f5772660cd758bfead95239861d54e86641ff
Samuel Marques <samuelcabralmarques@gmail.com>
2020-10-26 20:40:12 -0300

    Tela de cadastar

---
+++ src/pages/Cadastrar/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import ProffyBanner from '../../components/ProffyBanner';
+import AuthInput from '../../components/AuthInput';
+import AuthButton from '../../components/AuthButton';
+
+import './styles.css';
+import { Link } from 'react-router-dom';
+
+function Cadastrar() {
+
+  return (
+    <div className="page-cadastar">
+      <ProffyBanner/>
+      
+      <main>
+        <form>
+          
+        </form>
+      </main>
+
+    </div>
+  )
+
+}
+
+export default Cadastrar;
--- src/routes.jsx
+++ src/routes.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 // import Home from './pages/Home';
 import Login from './pages/Login';
+import Cadastrar from './pages/Cadastrar';
 
 function Routes() {
   return(
     <BrowserRouter>
       <Route path="/" exact component={Login} />
+      <Route path="cadastrar" exact component={Cadastrar} />
       {/* <Route path="/home" component={Home} /> */}
     </BrowserRouter>
   );
